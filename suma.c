cb3f6bca19abd3f2e2a69dbe49f49555dc046427
Andradeudla <daniel.andrade.rosillo@udla.edu.ec>
2023-12-03 12:53:22 -0500

    tercer commit

--- suma.c
+++ suma.c
@@ -4,38 +4,36 @@
 
 void sumamatrix(int fila, int columna, int matrixA[fila][columna], int matrixB[fila][columna], int matrixS[fila][columna]) // Definición de la función con void para que no devuelva ningun valor y para sumar matrices
 {
-    for (int i = 0; i < fila; i++)//realizamos el primer for para la matriz fila
+    for (int i = 0; i < fila; i++) // realizamos el primer for para la matriz fila
     {
-        for (int j = 0; j < columna; j++)//realizamos el segundo for para la matriz columna
+        for (int j = 0; j < columna; j++) // realizamos el segundo for para la matriz columna
         {
-            matrixS[i][j] = matrixA[i][j] + matrixB[i][j];// sumamos los elementos correspondisntes a las matrices A y B, y se almacenan el resultado en la matriz S
+            matrixS[i][j] = matrixA[i][j] + matrixB[i][j]; // sumamos los elementos correspondisntes a las matrices A y B, y se almacenan el resultado en la matriz S
         }
-        
     }
-    
 }
 
 int main() // la variable principal del programa de inicializacion
 {
     printf("----------Bienvenido al Programa de Suma de Matrices con Funciones---------- \n"); // se agrega un titulo para dar mejor presentacion
 
-    int fila, columna;                                // se define las variables para filas y columnas
+    int fila, columna;                       // se define las variables para filas y columnas
     printf("ingrese el valor para filas: "); // se solicita al usuario ingresar el numero de filas
-    scanf("%d", &fila);                         // se lee los datos ingresados por el usuario
+    scanf("%d", &fila);                      // se lee los datos ingresados por el usuario
 
     printf("ingrese el valor para columnas: "); // se solicita al usuario ingresar el numero de columnas
-    scanf("%d", &columna);                            // se lee los datos ingresados por el usuario
+    scanf("%d", &columna);                      // se lee los datos ingresados por el usuario
 
-    int matrixA[fila][columna];  // se define las variables con el numero de espacios ingresados por el usuario de filas y columnas.
+    int matrixA[fila][columna]; // se define las variables con el numero de espacios ingresados por el usuario de filas y columnas.
     int matrixB[fila][columna]; // se define las variables de la matriz B con el numero de espacios ingresados por el usuario.
     int matrixS[fila][columna]; // se define las variables de la matriz suma.
-    srand(time(NULL)); // se crea la funcion para numeros randoms
+    srand(time(NULL));          // se crea la funcion para numeros randoms
 
-    printf("---Esta es la Matriz A---");
+    printf("---Esta es la Matriz A---"); // se genera la matriz A con numeros aleatorios
 
     for (int i = 0; i < fila; i++) // se crea el primerfor para la matriz original de las filas
     {
-        printf("\n");               // se coloca para separa las filas
+        printf("\n");                     // se coloca para separa las filas
         for (int j = 0; j < columna; j++) // Se crea en segundo for para la matriz original de columnas
         {
             matrixA[i][j] = rand() % 100 + 1; // Se define que la matriz se ponga el rango de numeros random que se solicitan en cada cuadro de la matriz
@@ -43,12 +41,12 @@ int main() // la variable principal del programa de inicializacion
         }
     }
 
-        printf("\n");
-        printf("---Esta es la Matriz B---");
+    printf("\n");
+    printf("---Esta es la Matriz B---");
 
     for (int i = 0; i < fila; i++) // se crea el primerfor para la matriz original de las filas
     {
-        printf("\n");               // se coloca para separa las filas
+        printf("\n");                     // se coloca para separa las filas
         for (int j = 0; j < columna; j++) // Se crea en segundo for para la matriz original de columnas
         {
             matrixB[i][j] = rand() % 100 + 1; // Se define que la matriz se ponga el rango de numeros random que se solicitan en cada cuadro de la matriz
@@ -56,6 +54,7 @@ int main() // la variable principal del programa de inicializacion
         }
     }
 
+    sumamatrix(fila, columna, matrixA, matrixB, matrixS); // Llamamos a la función para sumar las matrices A y B
     printf("\n");
     printf("---Esta es la Matriz S---");
     for (int i = 0; i < fila; i++) // se crea el primer for para la matriz suna.
@@ -63,8 +62,8 @@ int main() // la variable principal del programa de inicializacion
         printf("\n");
         for (int j = 0; j < columna; j++) // se crea el segundor for para la matriz columna.
         {
-            matrixS[i][j] = matrixA[i][j] + matrixB[i][j];// se realiza el operador para la suma de las matrices
-            printf("%d \t", matrixS[i][j]);// se inprime la matriz suma.
+            matrixS[i][j] = matrixA[i][j] + matrixB[i][j]; // se realiza el operador para la suma de las matrices
+            printf("%d \t", matrixS[i][j]);                // se inprime la matriz suma.
         }
     }
 
