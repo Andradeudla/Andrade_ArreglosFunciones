058e87c0ed1284ecc41467d737fbd483effba4bc
Andradeudla <daniel.andrade.rosillo@udla.edu.ec>
2023-12-03 19:23:06 -0500

    segundo commit

--- tridimensional.c
+++ tridimensional.c
@@ -1,41 +1,31 @@
 #include <stdio.h>
 
-int main()
+void crearmatrix(int m, int f, int c, int matriz[f][c]); // definimos la funcion para crear la matriz tridimensional y usamos void para que no de retorno//
+
+for (int i = 0; i < m; i++) // se pone el primer for para la matriz
 {
-    int m, f, c; // Se define las variables que vamos a usar
-
-    printf("Ingrese el numero de matrices que desea ingresar: "); // Se pide al usuario las dimensiones, filas y columnas
-    scanf("%d", &m);
-    printf("Ingrese el numero de columnas de las matrices: ");
-    scanf("%d", &c);
-    printf("Ingrese el numero de filas de las matrices: ");
-    scanf("%d", &f);
-
-    int matriz[f][c]; // se define el espacio de cada variable 
-
-    for (int i = 1; i < m; i++) // La variable de i se inicializa con 1 porque ya se va a usar una matriz fija al final llena de 1
-
+    printf("\n Matriz %d", i);  // inprime el numero de la matriz actual
+    for (int j = 0; j < f; j++) // se pone el segundo for para las filas de la matriz
     {
-        printf("\n Matriz %d", i);  // El primer for para ver cuantas matrices se va a utilizar
-        for (int j = 0; j < f; j++) // Segundo for para ver las filas de las matrices
+        printf("\n");               // se inprime la matriz de forma espaciada
+        for (int k = 0; k < c; k++) // se pone el tercer for para las columnas
         {
-            printf("\n");
-            for (int k = 0; k < c; k++) // tercer for para ver las columnas de las matrices
-            {
-                matriz[j][k] = 0;              // Se llena las matrices de 0
-                printf("%d \t", matriz[j][k]); // Se imprime las matrices
-            }
+            matriz[j][k] = 0;              // Asignamos el valor 0 a cada elemento
+            printf("%d \t", matriz[j][k]); // Imprimimos el valor del elemento actual
         }
     }
-    printf("\n Matriz %d", m); // se imprime el numero de cada matriz segun ingreso el usuario
-    for (int j = 0; j < f; j++)// Se crea un ultimo for para la ultima matriz llena de 1
+}
+
+printf("\n Matriz %d", m); // Imprimimos la última matriz//
+for (int j = 0; j < f; j++)
+{
+    printf("\n");
+    for (int k = 0; k < c; k++)
     {
-        printf("\n");
-        for (int k = 0; k < c; k++)
-        {
-            matriz[j][k] = 1;// Se llena de unos la matriz
-            printf("%d \t", matriz[j][k]);// Se imprime la matriz de 1 al final
-        }
+        matriz[j][k] = 1;              // Asignamos el valor 1 a cada elemento
+        printf("%d \t", matriz[j][k]); // Imprimimos el valor del elemento actual
     }
+}
+
     return 0;
 }
